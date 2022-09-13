c19201d91ca49d92e2468e7a47963067e0e827ee
noumanshah436 <noumanshah436@gmail.com>
2022-09-13 15:35:20 +0500

    post index and create action completed

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.gem 'carrierwave', :version => '~> 2.0'
-config.gem 'cloudinary'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   devise_for :accounts
   resources :accounts, only: [:show]
 
   root to: "public#homepage"
+
+  resources :posts, only: [:index, :show, :create, :destroy] do
+    resources :photos, only: [:create]
+  end
+
 end
