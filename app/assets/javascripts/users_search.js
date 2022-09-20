1261d2259a98c0146575072c611885d442fba0ee
noumanshah436 <noumanshah436@gmail.com>
2022-09-20 19:29:50 +0500

    testing

--- app/assets/javascripts/users_search.js
+++ app/assets/javascripts/users_search.js
@@ -1,24 +1,28 @@
 $(document).ready(function(){
   $("#users-search #keyword").on("keyup", function(){
 
-    var jqxhr = $.get(
+    var ajax_result = $.get(
 
       $("#users-search").attr("action"),  // url where to get data
 
       {keyword: $("#users-search #keyword").val()},   //  pas data to get request
 
-      function(){                                     // 
+      function(){                                     // on success
         var result = $("#users-result").html();
+        // console.log('result')
+        // console.log(result)
+
         if(!result){
+
           $("#users-search #keyword").popover({
             content: "No result found.",
             placement: "bottom",
             html: true
-
           });
+
         } else {
-          console.log("Hey");
-          console.log($("#users-result"));
+          // console.log("Hey");
+          // console.log($("#users-result"));
           $("#users-search #keyword").popover({
             content: $("#users-result"),
             placement: "bottom",
@@ -29,5 +33,27 @@ $(document).ready(function(){
       }
     )
 
+
   })
 });
+
+
+
+
+// popover will generate/add this html:
+
+//  <div class="popover fade show bs-popover-bottom" role="tooltip" id="popover838875" x-placement="bottom"
+//   style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(1694px, 47px, 0px);">
+
+//   <div class="arrow" style="left: 56px;"></div>
+//   <h3 class="popover-header"></h3>
+//   <div class="popover-body">
+
+//     <div id="users-result">
+//       <div class="p-3">
+//         No results found.
+//       </div>
+
+//     </div>
+//   </div>
+// </div>
