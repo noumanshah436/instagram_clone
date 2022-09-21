dcc419fb43009ffcd6509730655505ee7383b2fd
noumanshah436 <noumanshah436@gmail.com>
2022-09-21 21:35:17 +0500

    show all stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,7 +2,7 @@ class StoriesController < ApplicationController
   before_action :authenticate_account!
 
   def index
-    # @posts = Post.all.includes(:photos, :account, :likes).order("id desc")
+    @stories = Story.all.includes(  :account).order("id desc")
     @story = Story.new
   end
 
@@ -49,3 +49,8 @@ end
 # "authenticity_token"=>"yrCzTGUYviuDV9qFC9dpzUE2vpCu5IOYmGIUv+WjYep1aeeLm3Lqv6c/3dR7u29RR0PfJhjIuUNRjE6Sybpg0g==",
 # "story"=>{ "image"=>#<ActionDispatch::Http::UploadedFile:0x00007f06ac158290 @tempfile=#<Tempfile:/tmp/RackMultipart20220921-104456-izk6w0.jpg>, @original_filename="2022-09-19-230459.jpg", @content_type="image/jpeg", @headers="Content-Disposition: form-data; name=\"story[image]\"; filename=\"2022-09-19-230459.jpg\"\r\nContent-Type: image/jpeg\r\n">, "image_cache"=>""}, "commit"=>"Add Story",
 # "controller"=>"stories", "action"=>"create", "account_id"=>"4"} permitted: false>
+
+
+# Parameters: {"utf8"=>"✓", "authenticity_token"=>"+AUyf0S7PnU+QG4Cr0z1k1uKu8b1xs7QQlDuLARPpMRH3Ga4utFq4RooaVPfIPMPXf/acEPq9AuLvrQBKFal/A==",
+# "story"=>{"image"=>#<ActionDispatch::Http::UploadedFile:0x00007ff0844b6938 @tempfile=#<Tempfile:/tmp/RackMultipart20220921-187639-hbz8a4.png>, @original_filename="Screenshot from 2022-09-08 16-37-17.png", @content_type="image/png", @headers="Content-Disposition: form-data; name=\"story[image]\"; filename=\"Screenshot from 2022-09-08 16-37-17.png\"\r\nContent-Type: image/png\r\n">, "image_cache"=>""},
+# "commit"=>"Add Story", "account_id"=>"4"}
