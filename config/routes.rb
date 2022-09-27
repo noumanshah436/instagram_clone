467fb39d3852319f7f8a62a15bc5ff93e43c6555
noumanshah436 <noumanshah436@gmail.com>
2022-09-27 14:19:54 +0500

    remove errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+  
   devise_for :accounts
 
   # resources :accounts, only: %i[index show]
@@ -23,8 +25,7 @@ Rails.application.routes.draw do
     root to: "devise/sessions#new"
   end
 
-  match "/404", to: "errors#not_found", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
+
 
 end
 
