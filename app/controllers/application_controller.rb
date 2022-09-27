95bbf2fd042e7561e453048b08f89583041c7868
noumanshah436 <noumanshah436@gmail.com>
2022-09-27 11:43:16 +0500

    error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,12 @@ class ApplicationController < ActionController::Base
   # *********************************
 
   # for pundit
-  # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  # private
+  private
 
-  # def user_not_authorized
-  #   flash[:alert] = "You are not authorized to perform this action."
-  #   redirect_back(fallback_location: root_path)
-  # end
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back(fallback_location: root_path)
+  end
 end
