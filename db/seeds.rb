b1eefaa2c8dc78165ee2d23e79cfaa89810ca324
noumanshah436 <noumanshah436@gmail.com>
2022-09-29 17:48:49 +0500

    seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 
-account = Account.create(email: "anasdx1590@gmail.com", name: "Anas", password: "nouman" , confirmed_at: DateTime.now)
-
-
-post = account.posts.new(content: "new post")
-post.save
-
+account1 = Account.create(email: "nabeel@gmail.com", name: "Nabeel", password: "nouman" , confirmed_at: DateTime.now)
+post = account1.posts.create(content: "new post")
 post.photos.create(image: File.open("#{Rails.root}/app/assets/images/default.png") )
+account1.likes.create(post_id: post.id)
+account1.comments.create(post_id: post.id, content:"New comment")
 
-account.likes.create({post_id: post.id})
 
-account.comments.create({post_id: post.id, content:"New comment"})
+account2 = Account.create(email: "shah@gmail.com", name: "Shah", password: "nouman" , confirmed_at: DateTime.now)
+post = account2.posts.create(content: "new post")
+post.photos.create(image: File.open("#{Rails.root}/app/assets/images/default2.png") )
+account2.likes.create(post_id: post.id)
+account2.comments.create(post_id: post.id, content:"New comment")
+
+
+account3 = Account.create(email: "muaaz@gmail.com", name: "Muaaz", password: "nouman" , confirmed_at: DateTime.now)
+post = account3.posts.create(content: "new post")
+post.photos.create(image: File.open("#{Rails.root}/app/assets/images/default3.png") )
+account3.likes.create(post_id: post.id)
+account3.comments.create(post_id: post.id, content:"New comment")
