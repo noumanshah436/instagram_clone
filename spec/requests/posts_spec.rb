33a2c480bd7fd8ebac0edf0f22386150ca74b4de
noumanshah436 <noumanshah436@gmail.com>
2022-10-05 13:15:06 +0500

    show edit index action tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,15 +2,48 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller  do
   let(:account) { create :account }
-  let(:postt) { create :post }
+  let(:post) { create :post }
 
-  it 'GET index' do
-    sign_in(account)
-    get :index
-    expect(response).to render_template :index
+  describe 'when user enter any unknown id for show edit update' do
+    it 'Post should not exist' do
+      sign_in(account)
+      get :show, params: { id: "1234567" }
+      expect(flash[:alert]).to eq("Post not exist!")
+      expect(response).to redirect_to root_path
+    end
   end
 
-  # describe "GET /index" do
-  #   pending "add some examples (or delete) #{__FILE__}"
-  # end
-end
+  describe 'GET /index' do
+    it 'should get index template' do
+      sign_in(account)
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET /show' do
+    it 'should get show template' do
+      sign_in(account)
+      get :show, params: { id: post.id }
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET /edit' do
+    it 'should get edit template' do
+      sign_in(post.account)
+      get :edit, params: { id: post.id }
+      expect(response).to render_template :edit
+    end
+  end
+
+
+
+
+end  #  end testing controller
+
+
+
+# describe "GET /index" do
+#   pending "add some examples (or delete) #{__FILE__}"
+# end
