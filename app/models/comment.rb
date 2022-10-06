b5819705a014dc7ccbf28e8e72c230ceb8eeb366
noumanshah436 <noumanshah436@gmail.com>
2022-10-06 13:17:46 +0500

    error handling + notes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :account
-  
+
   belongs_to :parent, class_name: 'Comment', optional: true
-  has_many :comments, -> { order(id: :desc) }, foreign_key: :parent_id, dependent: :delete_all
+  has_many :comments,  foreign_key: :parent_id, dependent: :delete_all
 
   validates :content, presence: true, length: { maximum: 500 }
 end
